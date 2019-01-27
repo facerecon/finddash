c1f1c70f2a46dc235fa04407e850d4daef3a9d36
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-01-27 13:02:10 -0800

    work

--- viewer.py
+++ viewer.py
@@ -4,17 +4,19 @@ import base64
 import numpy as np
 
 ip = input("please enter ip: ")
-
+# ip ='10.19.129.177'
 context = zmq.Context()
 footage_socket = context.socket(zmq.SUB)
-footage_socket.bind('tcp://' + ip + ':5555')
 footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
+footage_socket.connect('tcp://' + ip + ':5555')
 
 while True:
     try:
         frame = footage_socket.recv_string()
         img = base64.b64decode(frame)
+        # print(img[:20])
         npimg = np.fromstring(img, dtype=np.uint8)
+        print(npimg[:20])
         source = cv2.imdecode(npimg, 1)
         cv2.imshow("Stream", source)
         cv2.waitKey(1)
